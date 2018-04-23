319415a91d261c5361baced16a7d8e8e6904cec2
tonaramos <gtonaramos@gmail.com>
2018-04-23 16:21:19 -0700

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,40 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
- //declre vsar to save output string
+// your code goes here
+//declre vsar to save output string
 //do simple cases number string undefined null empty
 //move on to array and objects. 
 
-var output = '';
-function helperFunction (object){
-  if (typeof obj === "number" || typeof obj === 'boolean') {
-    output += obj;
-  } else if (typeof obj === 'object' && obj === null) {
-    output += 'null';
-  } else if (typeof obj === 'string') {
-    output += '"'+obj+'"';
-  } else if (Array.isArray(obj)){
-    output = '[';
-    for (var i = 0; i < obj.length; i++){
-      if (i < obj.length - 1){
-        output += stringifyJSON(obj[i]) + ',';
-      }else{
-        output += stringifyJSON(obj[i]);
+  var output = '';
+  var helperFunction = function (object) {
+    if (typeof obj === 'number' || typeof obj === 'boolean') {
+      output += obj;
+    } else if (typeof obj === 'object' && obj === null) {
+      output += 'null';
+    } else if (typeof obj === 'string') {
+      output += '"' + obj + '"';
+    } else if (Array.isArray(obj)) {
+      output = '[';
+      for (var i = 0; i < obj.length; i++) {
+        if (i < obj.length - 1) {
+          output += stringifyJSON(obj[i]) + ',';
+        } else {
+          output += stringifyJSON(obj[i]);
+        }
       }
+      output += ']';
+    } else if (typeof obj === 'object') {
+      output += '{';
+      if (Object.values(obj).length > 0) {
+        for (var j in obj) {
+          if (obj[j] !== undefined && typeof obj[j] !== 'function') {
+            output += '"' + j + '"' + ':' + stringifyJSON(obj[j]) + ',';
+          }
+        }
+        if (output.length > 1) {
+          output = output.slice(0, output.length - 1);
+        }
+      }
+      output += '}';
     }
-    output += ']';
-  } else if (typeof obj === 'object'){
-    output += '{';
-    for (var j in obj){
-        output += '"' + j + '"' + ':' + stringifyJSON(obj[j]) + ',';
-    }
-    output = output.slice(0, output.length - 1)
-    output += '}'
-  }
-}
-helperFunction(obj);
+  };
+  helperFunction(obj);
 
-return output;
+  return output;
 
 };
